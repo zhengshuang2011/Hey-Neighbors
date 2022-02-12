d196d10f0454fcd9a03e685a72e4a69e90fad037
wentingzoe <ywtzoe@gmail.com>
2022-02-12 14:52:59 -0500

    Create siderbar(Header), reorginazed the pages and move them to Pagesfolder, updatet the pagepath in App.js

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,13 +1,9 @@
 import React from "react";
-import Map from "./components/Map/Map";
 
 export default function LandingPage() {
     return (
         <>
         <h1>Landing Page</h1>
-
-        <Map />
-
         <h1>hello</h1>
         </>
     )
