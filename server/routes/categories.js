2955a86a7f93cce9a1040577fc0e8a27e763fb9e
MustafaSKaya <mustafakya88@hotmail.com>
2022-02-12 15:11:34 -0500

    my previous changes wasnt pushed somehow so again pushing

---
+++ server/routes/categories.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM categories;`)
+      .then((data) => {
+        const categories = data.rows;
+        console.log(res.json({ categories }));
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+  return router;
+};
