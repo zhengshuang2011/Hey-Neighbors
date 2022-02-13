a5cf99d6dad90029f5d69c08d80e9b3e191bd78d
wentingzoe <ywtzoe@gmail.com>
2022-02-13 13:42:40 -0500

    Update Header css and create create event button

---
+++ client/src/components/EventButton/EventButton.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
+import ConnectWithoutContactIcon from "@mui/icons-material/ConnectWithoutContact";
+import "./EventButton.css"
+//import Button from '@mui/material/Button';
+
+function EventButton() {
+  return (
+
+    <button className='eventButton'>
+      Create Your Event
+      <ArrowCircleRightIcon className='arrowIcon' />
+    </button>
+  )
+}
+
+export default EventButton
+
+
+
