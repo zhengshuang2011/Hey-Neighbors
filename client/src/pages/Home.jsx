7c365648b1f0208531f105f4895f781475e028e6
wentingzoe <ywtzoe@gmail.com>
2022-02-14 18:21:17 -0500

    Close components use master branch

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,18 +9,18 @@ import { Grid } from "@material-ui/core";
 import SmallCard from '../components/SmallCard/SmallCard';
 import './home.css'
 
-  const [event, setEvent] = useState("")
+const [event, setEvent] = useState("")
 
 
-  useEffect(() => {
-      axios
-        .get("/api/events")
-        .then((data) => {
-        console.log(data.data.events);
-        setEvent([...data.data.events]);
-        })
-        .catch((err) => console.log(err));
-    }, []);
+useEffect(() => {
+  axios
+    .get("/api/events")
+    .then((data) => {
+      console.log(data.data.events);
+      setEvent([...data.data.events]);
+    })
+    .catch((err) => console.log(err));
+}, []);
 
 function Home() {
 
@@ -30,7 +30,7 @@ function Home() {
       <Grid item >
         <Header />
       </Grid>
-      
+
 
       <Grid item xs={12} sm={5} >
         <Grid container direction="column"
@@ -49,22 +49,23 @@ function Home() {
         </Grid>
       </Grid>
 
-      <img src={event[1].photo_image} alt="Girl in a jacket" width="400" height="300"/>
+      <img src={event[1].photo_image} alt="Girl in a jacket" width="400" height="300" />
       <Grid item xs={12} sm={4}>
 
 
-      <Grid item xs={12} sm={5}>
+        <Grid item xs={12} sm={5}>
 
-        <Grid container direction="column"
-          justifyContent="space-evenly"
-          alignItems="stretch">
-          <Grid item >
-            <EventButton />
-          </Grid>
-          <Grid item>
-            <Map />
-          </Grid>
+          <Grid container direction="column"
+            justifyContent="space-evenly"
+            alignItems="stretch">
+            <Grid item >
+              <EventButton />
+            </Grid>
+            <Grid item>
+              <Map />
+            </Grid>
 
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
