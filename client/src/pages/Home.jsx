712a9c86bd9816d2030e4ab305ecc4d75ca9ff77
MustafaSKaya <mustafakya88@hotmail.com>
2022-02-14 17:36:24 -0500

    uploading image with base64 encoding is implemented

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../components/Header/Header'
 import Map from "../components/Map/Map";
 import EventButton from '../components/EventButton/EventButton';
 import SearchBar from "../components/Search/searchBar";
+import axios from "axios";
 
 import { Grid } from "@material-ui/core";
 function Home() {
 
+  const [event, setEvent] = useState("")
 
+  useEffect(() => {
+      axios
+        .get("/api/events")
+        .then((data) => {
+        console.log(data.data.events);
+        setEvent([...data.data.events]);
+        })
+        .catch((err) => console.log(err));
+    }, []);
 
   return (
     <Grid container direction='row' justifyContent="space-between"
@@ -15,6 +26,7 @@ function Home() {
       <Grid item xs={0} sm={2} >
         <Header />
       </Grid>
+      
 
       <Grid item xs={12} sm={6} >
         <Grid container direction="column"
@@ -23,6 +35,7 @@ function Home() {
           <SearchBar />
         </Grid>
       </Grid>
+      <img src={event[1].photo_image} alt="Girl in a jacket" width="800" height="600"/>
       <Grid item xs={12} sm={4}>
         <Grid container direction="column"
           justifyContent="space-evenly"
