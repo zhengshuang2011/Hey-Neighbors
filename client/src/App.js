08b4fcef0dfaefa8c60a5ac75e4a35bfdb3429e4
Miranda Zheng <zhengshuang2011@gmail.com>
2022-02-14 23:59:23 +0000

    save for merge

--- client/src/App.js
+++ client/src/App.js
@@ -9,39 +9,43 @@ import Myevent from "./pages/Myevent";
 import CreateEvent from "./pages/CreateEvent";
 import Bookings from "./pages/Bookings";
 import Home from "./pages/Home";
+import { useState } from "react";
 
 function App() {
+  const [token, setToken] = useState();
+
   useEffect(() => {
     axios
       .get("/api/users")
       .then((data) => console.log(data.data.users))
       .catch((err) => console.log(err));
   }, []);
-// useEffect(() => {
-//     axios
-//       .get("/api/events")
-//       .then((data) => {
-//       console.log(data.data.events);
-//       setEvent([...data.data.events]);
-//       })
-//       .catch((err) => console.log(err));
-//   }, []);
 
-//   console.log(`current event is = ${JSON.stringify(event)}`);
+  // useEffect(() => {
+  //     axios
+  //       .get("/api/events")
+  //       .then((data) => {
+  //       console.log(data.data.events);
+  //       setEvent([...data.data.events]);
+  //       })
+  //       .catch((err) => console.log(err));
+  //   }, []);
 
-//   useEffect(() => {
-//     axios
-//       .get("/api/applications")
-//       .then((data) => console.log(data.data.applications))
-//       .catch((err) => console.log(err));
-//   }, []);
+  //   console.log(`current event is = ${JSON.stringify(event)}`);
 
-//   useEffect(() => {
-//     axios
-//       .get("/api/categories")
-//       .then((data) => console.log(data.data.categories))
-//       .catch((err) => console.log(err));
-//   }, []);
+  //   useEffect(() => {
+  //     axios
+  //       .get("/api/applications")
+  //       .then((data) => console.log(data.data.applications))
+  //       .catch((err) => console.log(err));
+  //   }, []);
+
+  //   useEffect(() => {
+  //     axios
+  //       .get("/api/categories")
+  //       .then((data) => console.log(data.data.categories))
+  //       .catch((err) => console.log(err));
+  //   }, []);
   return (
     <div>
       <BrowserRouter>
@@ -50,7 +54,11 @@ function App() {
 
           <Route exact path="/home" element={<Home />} />
 
-          <Route exact path="/signIn" element={<SignIn />} />
+          <Route
+            exact
+            path="/signIn"
+            element={<SignIn setToken={setToken} />}
+          />
 
           <Route exact path="/register" element={<Register />} />
 
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import "./SignIn.css";
+import PropTypes from "prop-types";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
 
-export default function SignIn() {
-    return (
-        <h1>Sign In Page</h1>
-    )
+export default function SignIn({ setToken }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  console.log(error);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = JSON.stringify({
+      email,
+      password,
+    });
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    axios
+      .post("/api/users/login", data, {
+        headers: headers,
+      })
+      .then((response) => {
+        console.log(response.data);
+        setToken(response.data.id);
+        setError("");
+      })
+      .catch((err) => {
+        console.log("Error : ", err);
+        setError("error");
+      });
+  };
+
+  const validateForm = () => {
+    return email.length > 0 && password.length > 0;
+  };
+
+  return (
+    <div className="login-wrapper">
+      <h1>Please Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Email</p>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          <p>Password</p>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        {error && <p>wrong information</p>}
+        <div>
+          <button type="submit" disabled={!validateForm}>
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
+
+SignIn.propTypes = {
+  setToken: PropTypes.func.isRequired,
+};
