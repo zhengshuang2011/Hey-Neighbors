e97092c149c0032050891306d2f19cdef377a6da
MustafaSKaya <mustafakya88@hotmail.com>
2022-02-16 14:11:40 -0500

    now events gets rendered on the map container dynamically

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,33 +1,86 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GoogleMapReact from "google-map-react";
-import { Paper, Typography, useMediaQuery } from "@material-ui/core";
-import LocationOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
-import Rating from "@material-ui/lab";
+import { Paper, Typography } from "@material-ui/core";
 import useMapStyles from "./MapStyles";
+import axios from "axios";
 
-export default function Map() {
+export default function Map({ events }) {
+  console.log("events in map.jsx = ", events);
 
-    const classes = useMapStyles();
-    const ottawaCoordinates = { lat: 45.4111700, lng: -75.6981200 };
+  const classes = useMapStyles();
+  const ottawaCoordinates = { lat: 45.41117, lng: -75.69812 };
 
-    return (
-        <div className={classes.mapContainer}>
-            <GoogleMapReact
-                bootstrapURLKeys={{ key: "AIzaSyA_itBW9kNLgs2Ef1vImn7JhCVcLsJ6smQ" }}
-                defaultCenter={ottawaCoordinates}
-                center={ottawaCoordinates}
-                defaultZoom={14}
-                margin={[50, 50, 50, 50]}
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyA_itBW9kNLgs2Ef1vImn7JhCVcLsJ6smQ" }}
+        defaultCenter={ottawaCoordinates}
+        center={ottawaCoordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+      >
+        {events?.map((event) => {
+          console.log("event.map = ", event);
+          return (
+            <div
+              className={classes.markerContainer}
+              lat={event.locationlatitude}
+              lng={event.locationlongitude}
             >
-                <div className={classes.markerContainer} lat={45.4111700} lng={-75.6981200}>
-                    <Paper elevation={3} className={classes.paper}>
-                        <Typography className={classes.typography} variant="subtitle2" gutterBottom>
-                            Concert
-                        </Typography>
-                        <img className={classes.pointer} src={"https://1409791524.rsc.cdn77.org/data/images/full/514212/kpopconcert-jpg.jpg"} alt={"Concert"} />
-                    </Paper>
-                </div>
-            </GoogleMapReact>
+              <Paper elevation={3} className={classes.paper}>
+                <Typography
+                  className={classes.typography}
+                  variant="subtitle2"
+                  gutterBottom
+                >
+                  {event.event_name}
+                </Typography>
+                <img
+                  className={classes.pointer}
+                  src={event.photo_image}
+                  alt={event.event_name}
+                />
+              </Paper>
+            </div>
+          );
+        })}
+        {/* <div className={classes.markerContainer} lat={45.41117} lng={-75.69812}>
+          <Paper elevation={3} className={classes.paper}>
+            <Typography
+              className={classes.typography}
+              variant="subtitle2"
+              gutterBottom
+            >
+              Concert
+            </Typography>
+            <img
+              className={classes.pointer}
+              src={
+                "https://1409791524.rsc.cdn77.org/data/images/full/514212/kpopconcert-jpg.jpg"
+              }
+              alt={"Concert"}
+            />
+          </Paper>
         </div>
-    )
-};
+        <div className={classes.markerContainer} lat={45.29981303104935} lng={-75.89353069112829}>
+          <Paper elevation={3} className={classes.paper}>
+            <Typography
+              className={classes.typography}
+              variant="subtitle2"
+              gutterBottom
+            >
+              Concert
+            </Typography>
+            <img
+              className={classes.pointer}
+              src={
+                "https://1409791524.rsc.cdn77.org/data/images/full/514212/kpopconcert-jpg.jpg"
+              }
+              alt={"Concert"}
+            />
+          </Paper>
+            </div>*/}
+      </GoogleMapReact>
+    </div>
+  );
+}
