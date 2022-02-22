ff147999d26e9eaf5c90184c303b63bdf428dd65
wentingzoe <ywtzoe@gmail.com>
2022-02-21 20:00:55 -0500

    hostpage Css resign

--- client/src/components/Map/MapStyles.js
+++ client/src/components/Map/MapStyles.js
@@ -2,13 +2,13 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles(() => ({
   paper: {
-    padding: "10px",
+    padding: "5px",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems:"center",
-    width:"15vh",
-    height:"15vh",
+    width:"10vh",
+    height:"10vh",
     borderRadius:"10px",
     boxShadow:" 0 0 11px #6980f379",
     "&:hover": {color:"white", backgroundColor: "#92a5ef", boxShadow:"0 3px 5px 2px rgba(237, 234, 234, 0.3)"}
@@ -25,12 +25,13 @@ export default makeStyles(() => ({
   },
   pointer: {
     cursor: "pointer",
-    width:"10vh",
-    height:"10vh",
+    width:"8vh",
+    height:"8vh",
     borderRadius:"20px",
   },
   typography: {
     fontSize: "0.5rem",
     lineHeight: 1.2,
+    textAlign:"center",
   } 
 }));
